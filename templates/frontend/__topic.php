470ab8c8ec788612908fde18bb4c23c4458b4a4b
Bart Roorda <bart@tuxion.nl>
2013-03-28 18:11:36 +0100

    Updated forum HTML + CSS. - Actually @Lutz1337 did this 8)

--- templates/frontend/__topic.php
+++ templates/frontend/__topic.php
@@ -1,22 +1,23 @@
-<article>
+<article class="topic-starter-topic">
   
-  <header>
-    <h2><?php echo $data->topic->title; ?></h2>
-    <small>Created <time pubdate datetime="<?php echo $data->starter->dt_created; ?>">at one point</time>.</small>
+  <header class="clearfix">
+    <h1><?php echo $data->topic->title; ?></h1>
+    <p><small>Created <time pubdate datetime="<?php echo $data->starter->dt_created; ?>">at one point</time>.</small></p>
   </header>
   
   <div class="topic-starter-content">
     <?php echo $data->starter->content; ?>
   </div>
   
-  <footer>
-    
+  <footer class="clearfix">
+    <a class="btn btn-mini btn-submit pull-right">Reageer op dit topic</a>
   </footer>
   
-  <!-- Replies. -->
-  <?php echo $data->replies; ?>
-  
 </article>
+
+<!-- Replies. -->
+<?php echo $data->replies; ?>
+
 
 <?php if(false): ?>
   <?php echo $data->replies; ?>
