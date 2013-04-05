f0474936a7d8386438a633a284ae7301f13cf2a6
Adrian Klingen <a.klingen@anime-magic.nl>
2013-04-05 21:18:25 +0200

    Multiple changes * CSS for forums and topics * HTML for forums and topics

--- templates/frontend/forum_page.php
+++ templates/frontend/forum_page.php
@@ -1,9 +1,11 @@
 <section class="forum-overview">
   
   <h1>Forums</h1>
-  <ul class="forum-overview">
+  <ul class="forum-overview nolist overview-list">
     <?php foreach($data->forums as $forum): ?>
-    <li><a href="<?php echo $forum->link; ?>"><?php echo $forum->title; ?></a></li>
+    <li class="forum-overview-item">
+      <a href="<?php echo $forum->link; ?>"><?php echo $forum->title; ?></a>
+    </li>
     <?php endforeach; ?>
   </ul>
   
--- templates/frontend/topics.php
+++ templates/frontend/topics.php
@@ -1,20 +1,84 @@
 <?php if($data->subforums->size() > 0): ?>
 
-  <h2>Sub forums</h2>
+  <h2 class="hidden">Sub forums</h2>
 
-  <ul>
-    <?php foreach($data->subforums as $forum): ?>
-    <li><a href="<?php echo $forum->link; ?>"><?php echo $forum->title; ?></a></li>
-    <?php endforeach; ?>
-  </ul>
-
+    <table class="table table-bordered forum-subforum-overview">
+      <thead>
+        <tr>
+          <th>Forum</th>
+          <th>Last post</th>
+          <th>Topics</th>
+          <th>Posts</th>
+        </tr>
+      </thead>
+      <tfoot></tfoot>
+      <tbody>  
+        <?php foreach($data->subforums as $forum): ?>
+        <tr class="forum-subforum-list">
+          <td class="forum-title">
+            <a href="<?php echo $forum->link; ?>"><?php echo $forum->title; ?></a>
+          </td>
+          <td class="forum-lastpost">
+            <a href="#">Last post title here</a><br />
+            <small><time pubdate datetime="<?php echo $data->dt_created; ?>">at one point</time> by <a class="lastpost-author" href="#">1337LutZ</a></small>
+          </td>
+          <td class="forum-topics">
+            2.503
+          </td>
+          <td class="forum-posts">
+            658.200
+          </td>
+        </tr>
+        <?php endforeach; ?>
+      </tbody>
+    </table>
 <?php endif; ?>
 
+<?php if($data->topics->size() > 0): ?>
+<h2 class="hidden">Topics</h2>
 
-<h2>Topics</h2>
-
-<ul>
-  <?php foreach($data->topics as $topic): ?>
-  <li><a href="<?php echo $topic->link; ?>"><?php echo $topic->title; ?></a></li>
-  <?php endforeach; ?>
-</ul>
+<table class="table table-bordered forum-topic-overview">
+  <thead>
+    <tr>
+      <th>Topic</th>
+      <th>Last post</th>
+      <th>Replies</th>
+      <th>Views</th>
+    </tr>
+  </thead>
+  <tfoot></tfoot>
+  <tbody>  
+    <?php foreach($data->topics as $topic): ?>
+    <tr class="forum-topic-list">
+      <td class="forum-title">
+        <div class="span7">
+          <a href="<?php echo $topic->link; ?>"><?php echo $topic->title; ?></a><br />
+          <small><time pubdate datetime="<?php echo $data->dt_created; ?>">at one point</time> by <a class="lastpost-author" href="#">1337LutZ</a></small>
+        </div>
+        <div class="pagination pagination-mini pagination-right pull-right hidden-phone">
+          <ul>
+            <li><a href="#">Prev</a></li>
+            <li><a href="#">1</a></li>
+            <li><a href="#">2</a></li>
+            <li><a href="#">3</a></li>
+            <li><a href="#">4</a></li>
+            <li><a href="#">5</a></li>
+            <li><a href="#">Next</a></li>
+          </ul>
+        </div>
+      </td>
+      <td class="forum-lastpost">
+        <a href="#">Last post title here</a>
+        <small><time pubdate datetime="<?php echo $data->dt_created; ?>">at one point</time> by <a class="lastpost-author" href="#">1337LutZ</a></small>
+      </td>
+      <td class="forum-views">
+        5.842
+      </td>
+      <td class="forum-replies">
+        356
+      </td>
+    </tr>
+    <?php endforeach; ?>
+  </tbody>
+</table>
+<?php endif; ?>
